f4c4ab674a6fae80b945da4ec105ace42277bdbc
Muhaimin Shihab <talk.shihab@gmail.com>
2024-11-16 18:24:59 +0600

    fix: npm build error (window)

--- app/components/layouts/Header.js
+++ app/components/layouts/Header.js
@@ -6,12 +6,12 @@ import Share from '../Share';
 const Header = () => {
     return (
         <header className='bg-blue-500 text-white'>
-            <div className='container mx-auto py-8 px-4'>
+            <div className='container mx-auto py-4 px-4'>
                 <div className='flex justify-between items-center gap-2'>
                     <Link href='/' className='w-1/2 flex items-center gap-3 logo-container'>
-                        <div className='w-10 h-10 bg-white rounded-full overflow-hidden'>
+                        {/* <div className='w-10 h-10 bg-white rounded-full overflow-hidden'>
                             <img src='/assets/author.jpg' className='object-cover' alt='Logo' />
-                        </div>
+                        </div> */}
                         <h3 className='text-2xl md:text-3xl font-bold'>{process.env.NEXT_PUBLIC_APP_NAME}</h3>
                     </Link>
                     <div className='w-1/2 flex justify-end items-center gap-4'>
@@ -21,7 +21,7 @@ const Header = () => {
                                 <path d="M20 11C20 6.02944 15.9706 2 11 2C6.02944 2 2 6.02944 2 11C2 15.9706 6.02944 20 11 20C15.9706 20 20 15.9706 20 11Z" stroke="currentColor" strokeWidth="1.5" strokeLinejoin="round" />
                             </svg>
                         </Link>
-                        <Share link={window.location.origin} />
+                        <Share link={process.env.NEXT_PUBLIC_APP_URL} />
                     </div>
                 </div>
 
