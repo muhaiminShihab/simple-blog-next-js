85bc5b8b29430028e1b060493f904e649390ff3a
Muhaimin Shihab <talk.shihab@gmail.com>
2024-12-02 21:44:36 +0600

    hide search button

--- app/components/layouts/Header.js
+++ app/components/layouts/Header.js
@@ -27,12 +27,12 @@ const Header = async () => {
                         <h3 className='text-2xl md:text-3xl font-bold'>{siteData.title}</h3>
                     </Link>
                     <div className='w-1/2 flex justify-end items-center gap-4'>
-                        <Link href='#' className='hover:bg-[hsla(0,0%,100%,.2)] rounded-full p-2 transition-all'>
+                        {/* <Link href='#' className='hover:bg-[hsla(0,0%,100%,.2)] rounded-full p-2 transition-all'>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#FFFFFF" fill="none">
                                 <path d="M17.5 17.5L22 22" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                                 <path d="M20 11C20 6.02944 15.9706 2 11 2C6.02944 2 2 6.02944 2 11C2 15.9706 6.02944 20 11 20C15.9706 20 20 15.9706 20 11Z" stroke="currentColor" strokeWidth="1.5" strokeLinejoin="round" />
                             </svg>
-                        </Link>
+                        </Link> */}
                         <div className='p-2 hover:bg-[hsla(0,0%,100%,.2)] rounded-full transition-all'>
                             <Share link={process.env.NEXT_PUBLIC_APP_URL} withText={false} />
                         </div>
